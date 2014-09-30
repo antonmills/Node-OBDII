938b96a9476b2104d79df8052c8bd3a2e7b2a2eb
Anton Mills <hello@antonmills.com>
2014-09-30 10:12:52 +1000

    Small tidy-up

--- node-app/controllers/controller_tcpclient.js
+++ node-app/controllers/controller_tcpclient.js
@@ -1,46 +1,58 @@
 // requires
 var net = require("net");
 var client = new net.Socket();
-var utf8 = require('utf8');
+var utf8 = require("utf8");
+var Q = require("q");
 
 // mode parsers
 var mode01 = require(appRoot + "/mode/mode1parse");
 
 
+exports.clientConnect = function()
+{
+	var deferred = Q.defer;
+
+	// initial connection
+	client.connect(config['tcp-port'], config['tcp-ip'], function()
+	{
+		console.log("Connecting");
+	});
+}
+
 
 // initial connection
-client.connect(config['tcp-port'], config['tcp-ip'], function()
-{
-	console.log("Connecting");
-});
-
-// on connect, issue ATZ init
-client.on("connect", function(data)
-{
-	console.log("Connected, handshake in progress");
-	client.write("ATZ" + '\r');
-});
-
-// communication parsing
-client.on("data", function(buffer)
-{
-	var str = buffer.toString('utf8'); // note: has to be utf8, this took me forever to find out.
-	if(str.indexOf('>') > -1)
-	{
-		// test engine coolant temperature
-		client.write("0105" + '\r');
-	} else
-	{
-		var newdata = clean(str);
-		console.log(newdata.res + " temperature: " + parse(newdata.res) + " degrees");
-	}
-});
-
-// closing connection
-client.on("close", function()
-{
-	console.log("Connection closed");
-});
+//client.connect(config['tcp-port'], config['tcp-ip'], function()
+//{
+//	console.log("Connecting");
+//});
+//
+//// on connect, issue ATZ init
+//client.on("connect", function(data)
+//{
+//	console.log("Connected, handshake in progress");
+//	client.write("ATZ" + '\r');
+//});
+//
+//// communication parsing
+//client.on("data", function(buffer)
+//{
+//	var str = buffer.toString('utf8'); // note: has to be utf8, this took me forever to find out.
+//	if(str.indexOf('>') > -1)
+//	{
+//		// test engine coolant temperature
+//		client.write("0105" + '\r');
+//	} else
+//	{
+//		var newdata = clean(str);
+//		console.log(newdata.res + " temperature: " + parse(newdata.res) + " degrees");
+//	}
+//});
+//
+//// closing connection
+//client.on("close", function()
+//{
+//	console.log("Connection closed");
+//});
 
 
 
@@ -87,16 +99,16 @@ parse = function(d)
 
 
 // for testing calculations only
-var tmp = parse("41 04 4D"); // load
-console.log(tmp[0]);
-console.log(tmp[1]);
-
-var tmp = parse("41 05 48"); // temp
-console.log(tmp[0]);
-console.log(tmp[1]);
-
-var tmp = parse("41 00 9E 3E B8 11"); // check if pids 1-20 are available
-console.log(tmp[0]);
-
-var tmp = parse("41 20 80 00 00 00"); // check if pids 21-40 are available
-console.log(tmp[0]);
+//var tmp = parse("41 04 4D"); // load
+//console.log(tmp[0]);
+//console.log(tmp[1]);
+//
+//var tmp = parse("41 05 48"); // temp
+//console.log(tmp[0]);
+//console.log(tmp[1]);
+//
+//var tmp = parse("41 00 9E 3E B8 11"); // check if pids 1-20 are available
+//console.log(tmp[0]);
+//
+//var tmp = parse("41 20 80 00 00 00"); // check if pids 21-40 are available
+//console.log(tmp[0]);
